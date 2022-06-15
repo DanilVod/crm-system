74b8d1aa7da3017edc171032c03f7d539ddfdefd
DanilVod <danvodopyanov@yandex.ru>
2022-06-15 15:17:33 +0300

    fix build branch

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Typography, Button } from '@/components';
 import { getContacts } from '@/services/contactsService';
 export const Contacts = () => {
-  console.log(getContacts().then((res) => console.log(res.data[0].sales)));
+  console.log(getContacts().then((res) => console.log(res.data[0])));
   return (
     <div>
       <Button>
